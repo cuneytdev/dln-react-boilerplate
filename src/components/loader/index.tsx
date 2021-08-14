90f1559423b1e25db410654462e68c4256f55d35
cuneytdalan <cuneytdalan17@gmail.com>
2021-08-14 12:04:19 +0300

    typescript added.

--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -1,16 +1,17 @@
+import {FunctionComponent} from "react";
+
 import {LoadingOutlined} from "@ant-design/icons";
 import {Spin} from "antd";
-import PropTypes from "prop-types";
 
-export function Loader(props) {
+type LoaderType = {
+    message: string;
+}
+
+export const Loader: FunctionComponent<LoaderType> = (props) => {
     const {message} = props;
     const antIcon = <LoadingOutlined style={{fontSize: 24}} spin/>;
 
     return <div className="loader-container">
         <Spin indicator={antIcon}>{message}</Spin>
     </div>
-}
-
-Loader.propTypes = {
-    message: PropTypes.string
 }
